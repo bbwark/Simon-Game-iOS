5c85317fd7e254ed98d711065d8a3bad98f4cd08
bbwark <96076911+bbwark@users.noreply.github.com>
2022-11-05 16:55:37 +0100

    Creation and implementation of Start Button View (animated)

---
+++ Simon Game iOS/Simon Game iOS/Simon Game iOS/Views/Game/GameView.swift
@@ -0,0 +1,56 @@
+//
+//  GameView.swift
+//
+//  Created by Bruno De Vivo on 28/10/22.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    @ObservedObject var model: ViewModel = ViewModel()
+    
+    var body: some View {
+        ZStack {
+            
+            VStack{
+                Text("\(model.points)")
+                    .font(.largeTitle)
+                
+                Spacer()
+                
+                HStack{
+                    TileView(model.tile[0])
+                    TileView(model.tile[1])
+                }
+                
+                HStack{
+                    TileView(model.tile[2])
+                    TileView(model.tile[3])
+                }
+                
+                Spacer()
+            }
+            if !model.inGame {
+                VStack {
+                    Spacer()
+                    Button {
+                        model.startRound()
+                    } label: {
+                        StartButtonView()
+                    }
+                    .disabled(model.inGame)
+                    .offset(y: -80.0)
+                }
+                .transition(.move(edge: .bottom))
+            }
+        }
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
+
+
---
+++ Simon Game iOS/Simon Game iOS/Simon Game iOS/Views/Game/StartButtonView.swift
@@ -0,0 +1,52 @@
+//
+//  StartButtonView.swift
+//  Simon Game iOS
+//
+//  Created by Bruno De Vivo on 04/11/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    
+    @State var shine = false
+    
+    var body: some View {
+        let frame = Rectangle()
+            .frame(width: 140.0, height: 50.0)
+            .cornerRadius(15.0)
+        
+        return ZStack {
+            frame.foregroundColor(.white)
+            ZStack{
+                ZStack {
+                    frame.foregroundColor(.blue)
+                    Text("Start")
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                }
+                Rectangle()
+                    .frame(width: 40.0, height: 50.0)
+                    .offset(x: shine ? -105 : 105)
+                    .opacity(0.2)
+                    .foregroundColor(.white)
+                    .blur(radius: 8.0)
+                    .blendMode(.destinationOut)
+            }.compositingGroup()
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 2.0).repeatForever(autoreverses: false)){
+                shine.toggle()
+            }
+        }
+    }
+}
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            StartButtonView()
+        }
+    }
+}
